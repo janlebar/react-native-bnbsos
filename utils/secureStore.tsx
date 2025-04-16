222ddd91181f9ae3d7e9f11c1ed4b8e7b8f08546
stefan jan lebar <janstefanlebar@gmail.com>
2025-04-16 20:57:49 +0200

    change to secureStore.tsx so web development works

--- utils/secureStore.tsx
+++ utils/secureStore.tsx
@@ -1,15 +1,44 @@
 import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 const TOKEN_KEY = "userToken";
 
 export const saveToken = async (token: string) => {
-  await SecureStore.setItemAsync(TOKEN_KEY, token);
+  if (Platform.OS === "web") {
+    localStorage.setItem(TOKEN_KEY, token);
+  } else {
+    await SecureStore.setItemAsync(TOKEN_KEY, token);
+  }
 };
 
 export const getToken = async (): Promise<string | null> => {
-  return await SecureStore.getItemAsync(TOKEN_KEY);
+  if (Platform.OS === "web") {
+    return localStorage.getItem(TOKEN_KEY);
+  } else {
+    return await SecureStore.getItemAsync(TOKEN_KEY);
+  }
 };
 
 export const deleteToken = async () => {
-  await SecureStore.deleteItemAsync(TOKEN_KEY);
+  if (Platform.OS === "web") {
+    localStorage.removeItem(TOKEN_KEY);
+  } else {
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+  }
 };
+
+// import * as SecureStore from "expo-secure-store";
+
+// const TOKEN_KEY = "userToken";
+
+// export const saveToken = async (token: string) => {
+//   await SecureStore.setItemAsync(TOKEN_KEY, token);
+// };
+
+// export const getToken = async (): Promise<string | null> => {
+//   return await SecureStore.getItemAsync(TOKEN_KEY);
+// };
+
+// export const deleteToken = async () => {
+//   await SecureStore.deleteItemAsync(TOKEN_KEY);
+// };
