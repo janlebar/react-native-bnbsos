bb35a2cc5a6420efe4e85acb3c9915b3308d080e
stefan jan lebar <janstefanlebar@gmail.com>
2025-04-16 21:43:41 +0200

    change to home.tsx, developing footerMenu.tsx

--- app/user/footerMenu.tsx
+++ app/user/footerMenu.tsx
@@ -16,7 +16,6 @@ export default function FooterMenu() {
   return (
     <View style={styles.container}>
       <Button title="Logout" onPress={handleLogout} />
-      {/* You can add more buttons here later */}
     </View>
   );
 }
@@ -29,3 +28,35 @@ const styles = StyleSheet.create({
     backgroundColor: "#eee",
   },
 });
+
+// import React from "react";
+// import { View, StyleSheet, TouchableOpacity } from "react-native";
+// import { deleteToken } from "../../utils/secureStore";
+// import { useRouter } from "expo-router";
+// import LogoutIcon from "../assets/icons/logout.svg";
+
+// export default function FooterMenu() {
+//   const router = useRouter();
+
+//   const handleLogout = async () => {
+//     await deleteToken();
+//     router.replace("/login");
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <TouchableOpacity onPress={handleLogout}>
+//         <LogoutIcon width={28} height={28} />
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flexDirection: "row",
+//     justifyContent: "space-around",
+//     padding: 16,
+//     backgroundColor: "#eee",
+//   },
+// });
